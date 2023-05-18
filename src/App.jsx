8594e04191c640aee95276cf5e33b6395e751ff4
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-05-18 18:59:33 -0300

    Passando props para o compoente

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 
-function App() {
+import {Post} from './components/Post'
 
+
+export default function App() {
 
   return (
     <>
-     <h1>Bora bora</h1>
+    <Post
+        author="Diego Fernandes"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+      />
+      <Post
+        author="Reginaldo Oliveira"
+        content="Passando as props"
+      />
     </>
   )
 }
 
-export default App
+
