d083c923173cc140d6e39a2fd2a5aebf6872d189
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-05-18 21:15:12 -0300

    Criação do header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
-import {Post} from './components/Post'
-
-
+import {Post} from './components/Post/Post'
+import './components/Header/Header';
+import { Header } from './components/Header/Header';
+import './styles.css';
 export default function App() {
 
   return (
     <>
+    <Header />
     <Post
         author="Diego Fernandes"
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
