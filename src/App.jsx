5a1bf5df7dfb7c47fe006f6f6635aee83609bc69
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-05-19 21:37:56 -0300

    Finalização do sidebar e instalação do phosphor-react

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
 
-import {Post} from './components/Post/Post'
+import {Post} from './components/Post/Post';
 
 import { Header } from './components/Header/Header';
+import { Sidebar} from './components/Sidebar/Sidebar';
 import './global.css';
+import styles from './App.module.css';
 export default function App() {
 
   return (
     <>
-    <Header />
-    <Post
-        author="Diego Fernandes"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-      />
-      <Post
-        author="Reginaldo Oliveira"
-        content="Passando as props"
-      />
+        <Header />
+        <div className={styles.wrapper}>
+        <Sidebar />
+            <main>
+                <Post
+                    author="Diego Fernandes"
+                    content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+                  />
+                  <Post
+                    author="Reginaldo Oliveira"
+                    content="Passando as props"
+                  />
+          </main>
+          </div>
     </>
   )
 }
