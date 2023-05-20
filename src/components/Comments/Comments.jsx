ae6f9757dd4129ab54338639bbf652b571bb72c6
Reginaldo Oliveira <reginaldo007oliveira@gmail.com>
2023-05-20 14:52:24 -0300

    adicionando contador

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from './Comments.module.css';
 import { Avatar } from '../Avatar/Avatar';
 import { ThumbsUp, Trash } from 'phosphor-react';
@@ -6,9 +7,16 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
   }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+  }
+  
   return (
     
     <div className={styles.comment}>
@@ -30,9 +38,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
